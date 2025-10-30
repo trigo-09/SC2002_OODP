0132da1804dc99a523bb44049a4ef96fc86537b1
Mega <Vmega2003@gmail.com>
2025-10-30 16:36:36 +0800

    remove status from internbuilder paramtere

--- src/entity/internship/InternBuilder.java
+++ src/entity/internship/InternBuilder.java
@@ -10,7 +10,7 @@ public class InternBuilder {
 
 	public static InternshipOpportunity createInternship(String id, String companyName, String title, String description, InternshipLevel level,
 														 String preferredMajors, LocalDate openingDate, LocalDate closingDate, int numOfSlots,
-														 InternStatus status, CompanyRep createdBy){
+														  CompanyRep createdBy){
 		InternshipOpportunity internshipOpportunity = new InternshipOpportunity(id, companyName, title, description, level, preferredMajors, openingDate,
 				closingDate, numOfSlots,createdBy);
 		return internshipOpportunity;
