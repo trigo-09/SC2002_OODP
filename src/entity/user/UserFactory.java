67b0cdcd4952eed7918f611834c1f389b1e8bd00
Gregger <seanmacinnes1234@gmail.com>
2025-11-02 17:05:20 +0800

    UserFactory.java
    switch function based on role
    
    Signed-off-by: Gregger <seanmacinnes1234@gmail.com>

--- src/entity/user/UserFactory.java
+++ src/entity/user/UserFactory.java
@@ -12,8 +12,24 @@ public class UserFactory {
 	 * @param attributes
 	 */
 	public User createUser(UserRole role, String id, String name, String hashedPassword, Map<String, String> attributes) {
-		// TODO - implement UserFactory.createUser
-		throw new UnsupportedOperationException();
-	}
+        switch(role) {
+            case STUDENT:
+                int year = Integer.parseInt(attributes.get("year"));
+                String major = attributes.get("major");
+                Student tempStudent = new Student(name,id,hashedPassword,year,major);
+                return tempStudent;
+            case STAFF:
+                String staffDepartment = attributes.get("department");
+                CareerStaff tempStaff = new CareerStaff(name,id,hashedPassword,staffDepartment);
+                return tempStaff;
+            case REP:
+                String company = attributes.get("company");
+                String repDept = attributes.get("department");
+                String position = attributes.get("position");
+                CompanyRep tempRep = new CompanyRep(name,id,hashedPassword,company,repDept,position,RepStatus.PENDING);
+                return tempRep;
+        }
+        return null;
+    }
 
 }
