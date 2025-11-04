2b85e073309ce95b082968f1785c1ae0d91eaa0a
Mega <Vmega2003@gmail.com>
2025-11-04 17:35:13 +0800

    changed to modern switch statment + add error throwing

--- src/entity/user/UserFactory.java
+++ src/entity/user/UserFactory.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class UserFactory {
 
+    private UserFactory() {} //prevent instantiation of class
+
 	/**
 	 * 
 	 * @param role
@@ -11,25 +13,26 @@ public class UserFactory {
 	 * @param hashedPassword
 	 * @param attributes
 	 */
-	public User createUser(UserRole role, String id, String name, String hashedPassword, Map<String, String> attributes) {
-        switch(role) {
-            case STUDENT:
+	public static User createUser(UserRole role, String id, String name, String hashedPassword, Map<String, String> attributes) {
+        if(attributes == null || attributes.isEmpty()) {throw new IllegalArgumentException("attributes is null or empty");}
+        return switch(role) {
+            case STUDENT -> {
                 int year = Integer.parseInt(attributes.get("year"));
                 String major = attributes.get("major");
-                Student tempStudent = new Student(name,id,hashedPassword,year,major);
-                return tempStudent;
-            case STAFF:
+                yield new Student(name, id, hashedPassword, year, major);
+            }
+            case STAFF-> {
                 String staffDepartment = attributes.get("department");
-                CareerStaff tempStaff = new CareerStaff(name,id,hashedPassword,staffDepartment);
-                return tempStaff;
-            case REP:
+                yield new CareerStaff(name, id, hashedPassword, staffDepartment);
+            }
+            case REP-> {
                 String company = attributes.get("company");
                 String repDept = attributes.get("department");
                 String position = attributes.get("position");
-                CompanyRep tempRep = new CompanyRep(name,id,hashedPassword,company,repDept,position,RepStatus.PENDING);
-                return tempRep;
-        }
-        return null;
+                yield new CompanyRep(name, id, hashedPassword, company, repDept, position);
+            }
+            default -> {throw new UnsupportedOperationException("Unsupported role");}
+        };
     }
 
 }
