be28361b713692981066c396017f07ba14da1427
Mega <Vmega2003@gmail.com>
2025-11-05 01:25:23 +0800

    implemented hashing method

--- src/util/PasswordHasher.java
+++ src/util/PasswordHasher.java
@@ -1,14 +1,33 @@
 package util;
+import java.security.NoSuchAlgorithmException;
+import java.security.MessageDigest;
+import java.util.List;
 
+/**
+ * Helps to hash password and verify them using the SHA3-256 algorithm
+ */
 public class PasswordHasher {
+
+    private static final String HASH_ALGORITHM = "SHA-256";
 
 	/**
 	 * 
 	 * @param password
+     * @return String
 	 */
 	public String hash(String password) {
-		// TODO - implement PasswordHasher.hash
-		throw new UnsupportedOperationException();
+        try {
+            MessageDigest md = MessageDigest.getInstance(HASH_ALGORITHM);
+            byte[] hashedPassword = md.digest(password.getBytes());
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : hashedPassword) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error hashing password",e);
+        }
 	}
 
 	/**
