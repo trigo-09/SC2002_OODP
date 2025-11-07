7cdeaff24fd5d1726c311a80cc492979751416a4
Mega <Vmega2003@gmail.com>
2025-11-07 15:03:57 +0800

    implemented SystemResposistory

--- src/controller/database/SystemResposistory.java
+++ src/controller/database/SystemResposistory.java
@@ -1,6 +1,7 @@
 package controller.database;
 
 import java.util.*;
+import java.util.stream.Stream;
 import entity.request.*;
 import entity.user.*;
 import entity.internship.InternshipOpportunity;
@@ -14,19 +15,16 @@ public class SystemResposistory implements IResposistory {
 	private final Map<String, CareerStaff> careerStaff = new HashMap<>();
 	private final Map<String, CompanyRep> pendingReps = new HashMap<>();
 	private final Map<String, CompanyRep> approvedReps = new HashMap<>();
-	private final List<InternshipOpportunity> internships = new ArrayList<>();
-	private final List<Application> applications = new ArrayList<>();
-    private final List<WithdrawalRequest> withdrawalRequests = new ArrayList<>();
-    private final List<RegistrationRequest> registrationRequests = new ArrayList<>();
-    private final List<InternshipVetRequest> internshipVetRequests = new ArrayList<>();
+    private final Map<String, Request> requests = new HashMap<>();
 
 	/**
 	 * 
 	 * @param app
+     * add application to both student and internship object
 	 */
-	public void addApplication(Application app) {
-		// TODO - implement SystemResposistory.addApplication
-		throw new UnsupportedOperationException();
+	public void addApplication(String studentId,Application app) {
+        students.get(studentId).addApplication(app);
+        findInternshipOpportunity(app.getInternshipId()).addPendingApplication(app);
 	}
 
 	/**
@@ -34,17 +32,15 @@ public class SystemResposistory implements IResposistory {
 	 * @param staff
 	 */
 	public void addCareerStaff(CareerStaff staff) {
-		// TODO - implement SystemResposistory.addCareerStaff
-		throw new UnsupportedOperationException();
+        careerStaff.put(staff.getId(), staff);
 	}
 
 	/**
 	 * 
 	 * @param intern
 	 */
-	public void addInternship(InternshipOpportunity intern) {
-		// TODO - implement SystemResposistory.addInternship
-		throw new UnsupportedOperationException();
+	public void addInternship(String repId,InternshipOpportunity intern) {
+        approvedReps.get(repId).addInternship(intern);
 	}
 
 	/**
@@ -52,8 +48,7 @@ public class SystemResposistory implements IResposistory {
 	 * @param student
 	 */
 	public void addStudent(Student student) {
-		// TODO - implement SystemResposistory.addStudent
-		throw new UnsupportedOperationException();
+        students.put(student.getId(), student);
 	}
 
     /**
@@ -61,7 +56,7 @@ public class SystemResposistory implements IResposistory {
      * @param withdrawalRequest
      */
     public void addWithdrawalRequest(WithdrawalRequest withdrawalRequest) {
-        throw new UnsupportedOperationException();
+        requests.put(withdrawalRequest.getId(), withdrawalRequest);
     }
 
     /**
@@ -69,7 +64,7 @@ public class SystemResposistory implements IResposistory {
      * @param registrationRequest
      */
     public void addRegistrationRequest(RegistrationRequest registrationRequest) {
-        throw new UnsupportedOperationException();
+        requests.put(registrationRequest.getId(), registrationRequest);
     }
 
     /**
@@ -77,75 +72,110 @@ public class SystemResposistory implements IResposistory {
      * @param internshipVetRequest
      */
     public void addInternshipVetRequest(InternshipVetRequest internshipVetRequest) {
-        throw new UnsupportedOperationException();
+        requests.put(internshipVetRequest.getId(), internshipVetRequest);
     }
 
     /**
      *
-     * @param withdrawalRequest
+     * @param requestId
      */
     @Override
-    public void removeWithdrawalRequest(WithdrawalRequest withdrawalRequest) {
-        throw new UnsupportedOperationException();
+    public void removeWithdrawalRequest(String requestId) {
+        requests.remove(requestId);
     }
 
     /**
      *
-     * @param internshipVetRequest
+     * @param requestId
      */
     @Override
-    public void removeInternshipVetRequest(InternshipVetRequest internshipVetRequest) {
-        throw new UnsupportedOperationException();
+    public void removeInternshipVetRequest(String requestId) {
+        requests.remove(requestId);
     }
 
     /**
      *
-     * @param registrationRequest
+     * @param requestId
      */
     @Override
-    public void removeRegistrationRequest(RegistrationRequest registrationRequest) {
-        throw new UnsupportedOperationException();
+    public void removeRegistrationRequest(String requestId) {
+        requests.remove(requestId);
     }
 
     /**
 	 * 
 	 * @param studentId
 	 */
-	public List<Application> appByStudent(String studentId) {
-		// TODO - implement SystemResposistory.appByStudent
-		throw new UnsupportedOperationException();
+	public List<Application> applicationByStudent(String studentId) {
+        Student student = students.get(studentId);
+        return (student != null) ? student.getApplications() : new ArrayList<>();
 	}
 
 	/**
 	 * 
 	 * @param internId
 	 */
-	public List<InternshipOpportunity> appForInternship(String internId) {
-		// TODO - implement SystemResposistory.appForInternship
-		throw new UnsupportedOperationException();
+	public List<Application> applicationForInternship(String internId) {
+        return approvedReps.values().stream()
+                .flatMap(rep -> rep.getInternships().stream())
+                .filter(internship -> internship.getId().equals(internId))
+                .findFirst()
+                .map(InternshipOpportunity::getPendingApplications)
+                .orElseGet(Collections::emptyList);
 	}
 
 	/**
 	 * 
 	 * @param repId
 	 */
-	public CompanyRep approveCompanyRep(String repId) {
-		// TODO - implement SystemResposistory.approveCompanyRep
-		throw new UnsupportedOperationException();
+	public void approveCompanyRep(String repId) {
+        CompanyRep rep = pendingReps.get(repId);
+        if (rep != null) {
+            pendingReps.remove(repId);
+            approvedReps.put(repId, rep);
+        }
+
 	}
+
+    public Request getRequest(String requestId) {
+        return requests.get(requestId);
+    }
 
 	/**
 	 * 
 	 * @param userId
 	 */
+    // might change it
 	public User findUser(String userId) {
-		// TODO - implement SystemResposistory.findUser
-		throw new UnsupportedOperationException();
+        return Stream.of(students, careerStaff, approvedReps, pendingReps)
+                .map(m -> m.get(userId))
+                .filter(Objects::nonNull)
+                .findFirst()
+                .orElse(null);
 	}
 
-	public List<Application> getApplications() {
-		return this.applications;
+    public InternshipOpportunity findInternshipOpportunity(String internshipId) {
+        return approvedReps.values().stream()
+                .flatMap(c -> c.getInternships().stream())
+                .filter(intern -> intern.getId().equals(internshipId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public Application findApplication(String applicationId) {
+        return students.values().stream()
+                .flatMap(s -> s.getApplications().stream())
+                .filter(app -> app.getApplicationId().equals(applicationId))
+                .findFirst()
+                .orElse(null);
+    }
+
+	public List<Application> getAllApplications() {
+        return students.values().stream()
+                .flatMap(s -> s.getApplications().stream())
+                .toList();
 	}
+
 
 	public Map<String, CompanyRep> getApprovedReps() {
 		return this.approvedReps;
@@ -155,8 +185,10 @@ public class SystemResposistory implements IResposistory {
 		return this.careerStaff;
 	}
 
-	public List<InternshipOpportunity> getInternships() {
-		return this.internships;
+	public List<InternshipOpportunity> getAllInternships() {
+		return approvedReps.values().stream()
+                .flatMap(rep->rep.getInternships().stream())
+                .toList();
 	}
 
 	public Map<String, CompanyRep> getPendingReps() {
@@ -167,23 +199,26 @@ public class SystemResposistory implements IResposistory {
 		return this.students;
 	}
 
-    public List<InternshipVetRequest> getInternshipVetRequests() {return this.internshipVetRequests;}
-
-    public List<RegistrationRequest> getRegistrationRequests() {return this.registrationRequests;}
-
-    public List<WithdrawalRequest> getWithdrawalRequests() {return this.withdrawalRequests;}
-
-    public void deleteRequest(Request request) {
-        throw new UnsupportedOperationException(); // to be implemented
+    public <T extends Request> List<T> getAllRequests(Class<T> type) {
+        return requests.values().stream()
+                .filter(type::isInstance)
+                .map(type::cast)
+                .toList();
     }
+
+    public void deleteApplication(String studentId, String applicationId) {
+        Optional.ofNullable(students.get(studentId))
+                .ifPresent(student -> student.withdrawApplication(applicationId));
+    }
+
+
 
 	/**
 	 * 
 	 * @param rep
 	 */
 	public void registerCompanyRep(CompanyRep rep) {
-		// TODO - implement SystemResposistory.registerCompanyRep
-		throw new UnsupportedOperationException();
+        pendingReps.put(rep.getId(), rep);
 	}
 
 }
