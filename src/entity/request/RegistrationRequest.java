8dd2dd737ff1cfdfb3823cce3815c4e83033901a
Mega <Vmega2003@gmail.com>
2025-11-07 14:47:32 +0800

    white space

--- src/entity/request/RegistrationRequest.java
+++ src/entity/request/RegistrationRequest.java
@@ -2,18 +2,15 @@ package entity.request;
 
 import entity.user.CompanyRep;
 import entity.user.RepStatus;
-import entity.user.User;
 
 public class RegistrationRequest extends Request {
     private final CompanyRep company;
    public RegistrationRequest(CompanyRep companyRep) {
        super(companyRep.getId());
        this.company = companyRep;
-
    }
 
    public void approve() {
-       this.status = RequestDecision.APPROVED;
        company.setStatus(RepStatus.REGISTERED);
        company.addNotification("Account has been approved");
    }
