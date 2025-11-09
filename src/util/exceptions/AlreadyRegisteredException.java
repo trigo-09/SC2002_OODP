cd7b377fd886ea427e2eff539a5b03848b6c4fee
Mega <Vmega2003@gmail.com>
2025-11-09 16:58:22 +0800

    added changepage class

---
+++ src/util/exceptions/AlreadyRegisteredException.java
@@ -0,0 +1,11 @@
+package util.exceptions;
+
+public class AlreadyRegisteredException extends Exception {
+    /**
+     * Constructs a new {@code AlreadyRegisteredException} with a default detail message.
+     * The default message is "This user ID is already registered."
+     */
+    public AlreadyRegisteredException() {
+        super("This user ID is already registered.");
+    }
+}
---
+++ src/util/ui/GraphicLogo.java
@@ -0,0 +1,4 @@
+package util.ui;
+
+public class Logo {
+}
