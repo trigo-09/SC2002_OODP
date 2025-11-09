cdf0b0c524faf4a14d114a469599945b1dda2946
Mega <Vmega2003@gmail.com>
2025-11-09 22:59:02 +0800

    90% of the class has been implemented

--- src/controller/database/SystemDataManager.java
+++ src/controller/database/SystemDataManager.java
@@ -1,21 +1,50 @@
 package controller.database;
 
+
+import java.io.*;
+
 public class SystemDataManager {
 
-	private static final String FILE_PATH = "";// filename to be filled
+	private final String filePath ;// filename to be filled
+    private static final String FILE_PATH = "system.dat";
 
-	public static SystemResposistory load() {
-		// TODO - implement SystemDataManager.load
-		throw new UnsupportedOperationException();
-	}
+    public SystemDataManager(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public SystemDataManager() {
+        this(FILE_PATH);
+    }
+
+
+	public IResposistory load() {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filePath));
+            return (IResposistory) ois.readObject();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+            return new SystemResposistory(); // to be swapped with CSV loader
+        } catch (IOException | ClassNotFoundException e) {
+            throw new RuntimeException("failed to load from .dat file", e);
+        }
+    }
+
 
 	/**
 	 * 
 	 * @param repo
 	 */
-	public static void save(IResposistory repo) {
-		// TODO - implement SystemDataManager.save
-		throw new UnsupportedOperationException();
+	public void save(IResposistory repo) {
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filePath));
+            oos.writeObject(repo);
+        }catch (IOException e){
+            throw new RuntimeException("failed to save to .dat file", e);
+        }
 	}
+
+    public void saveUpdate(IResposistory repo) {
+        save(repo);
+    }
 
 }
