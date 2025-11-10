6e05187510833104b5c6a7e6cace2a0a61a50ab7
Cristian Chye <cristianchye@gmail.com>
2025-11-10 15:15:03 +0800

    Coded the student controller class. Added a new method withdrawPlacement. Added a new method in the InternshipService class called getEligibleInternships which returns a list of internships that the student is eligible for. This will be called in the ableToApply method in the student controller class for the student to view the internships they can apply for

---
+++ src/boundary/StudentUI.java
@@ -0,0 +1,15 @@
+package boundary;
+
+import controller.database.SystemDataManager;
+
+import java.util.Scanner;
+
+public class StudentUI {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        do{
+
+        }
+    }
+}
