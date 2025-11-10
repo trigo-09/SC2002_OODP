ccef005dcae248ceb6a11c69fa13713b36aaab5d
Mega <Vmega2003@gmail.com>
2025-11-10 20:12:52 +0800

    typo

--- src/entity/application/ApplicationStatus.java
+++ src/entity/application/ApplicationStatus.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public enum ApplicationStatus implements Serializable {
     PENDING, //once created
-    APPROVED, // application accepte by rep
+    APPROVED, // application accepted by rep
     REJECTED, // application rejected by rep
     ACCEPTED, // application accepted by rep and student
     WITHDRAWN // student withdraw application
