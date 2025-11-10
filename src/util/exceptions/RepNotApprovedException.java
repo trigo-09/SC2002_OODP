4097a02a8e1c3305fd22e9b29cb2c5f533e0ced5
Mega <Vmega2003@gmail.com>
2025-11-10 20:31:48 +0800

    added and amend exception

--- src/util/exceptions/RepNotApprovedException.java
+++ src/util/exceptions/RepNotApprovedException.java
@@ -2,7 +2,7 @@ package util.exceptions;
 
 public class RepNotApprovedException extends AuthenticationException {
     public RepNotApprovedException(String repId) {
-        super("Company representative account " + repId + " has not been approved yet");
+        super("Company representative account " + repId + " has been rejected");
     }
 
 }
