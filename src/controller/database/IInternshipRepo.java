09d9b759ce4334b194bdb0ca783a5a97bc300c12
Mega <Vmega2003@gmail.com>
2025-11-10 15:22:43 +0800

    local changes

--- src/controller/database/IInternshipRepo.java
+++ src/controller/database/IInternshipRepo.java
@@ -18,7 +18,7 @@ public interface IInternshipRepo {
     /**
      *
      * @param companyName
-     * 
+     *
      */
 
     List<InternshipOpportunity> getInternshipsByCompany(String companyName);
