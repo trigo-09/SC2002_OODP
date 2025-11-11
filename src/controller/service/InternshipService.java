c9fd98b57706eec58ca6ef2e391b8592b77b78b7
Mega <Vmega2003@gmail.com>
2025-11-12 01:12:50 +0800

    change in para

--- src/controller/service/InternshipService.java
+++ src/controller/service/InternshipService.java
@@ -73,8 +73,8 @@ public class InternshipService {
 
 	/**
 	 * 
-	 * @param id
-	 * @param status
+	 * @param internship
+	 *
 	 */
 	private boolean ableToEditInternship(InternshipOpportunity internship){
         return internship.getStatus() == InternStatus.PENDING;
