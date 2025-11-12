d9a22b0d8fef84c6803270b9e843c9d56c8ca2e2
Mega <Vmega2003@gmail.com>
2025-11-13 00:39:53 +0800

    added new attribute

--- src/boundary/usermenu/RepMenuUI.java
+++ src/boundary/usermenu/RepMenuUI.java
@@ -14,6 +14,7 @@ import util.io.InputHelper;
   */
 public class RepMenuUI {
     private final RepController repController;
+    private final SystemController systemController;
 
      /**
       * Creates a representative menu bound to the given controller.
