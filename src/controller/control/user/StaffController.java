451f808e2797d91e0bd560eab6e4f252ccd89660
Mega <Vmega2003@gmail.com>
2025-11-12 11:44:43 +0800

    change to logic

--- src/controller/control/user/StaffController.java
+++ src/controller/control/user/StaffController.java
@@ -72,7 +72,7 @@ public class StaffController extends UserController {
 	 */
 	public void approveWithdrawal(String app) throws Exception {
 		getRequest().acceptWithdrawalRequest(app);
-        applicationService.removeApplicationInInternship(app);
+        internshipService.addApplicationToInternship(applicationService.findApplication(app));
 	}
 
 	/**
