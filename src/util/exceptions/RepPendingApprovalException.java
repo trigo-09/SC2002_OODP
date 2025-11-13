2f3e99f909fd117337c343c013dcda46b7c08f55
Mega <Vmega2003@gmail.com>
2025-11-14 00:50:31 +0800

    changes msg

--- src/util/exceptions/RepPendingApprovalException.java
+++ src/util/exceptions/RepPendingApprovalException.java
@@ -1,7 +1,11 @@
 package util.exceptions;
 
+
 public class RepPendingApprovalException extends AuthenticationException {
     public RepPendingApprovalException(String repId) {
-        super("account " + repId + " has been registered");
+        super("account " + repId + " has not been approved yet");
+    }
+    public RepPendingApprovalException(String repId, String message) {
+        super(repId + message);
     }
 }
