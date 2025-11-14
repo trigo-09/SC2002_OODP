de60d6d560c5ee67d189b79a1e28be642235d4d3
Mega <Vmega2003@gmail.com>
2025-11-14 22:57:56 +0800

    javadoc

--- src/boundary/FilterUI.java
+++ src/boundary/FilterUI.java
@@ -9,9 +9,20 @@ import util.io.InputHelper;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/**
+ * The FilterUI class manage the user interface for applying and modifying filter
+ * criteria for internship opportunities such as internship status, preferred major, internship level, and closing date.
+ */
 public class FilterUI {
     private static final DateTimeFormatter DMY = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
+    /**
+     * Updates the filter settings
+     * Displays a menu of criterias of the filter
+     * allows the user to clear all filters as well.
+     *
+     * @param filter The {@code FilterCriteria} object that stores the current filter settings.
+     */
     public static void update(FilterCriteria filter) {
         ChangePage.changePage();
         boolean back = false;
@@ -40,6 +51,10 @@ public class FilterUI {
         }
     }
 
+    /**
+     * Displays a menu of possible status options, accepts user input to select the status
+     * @param filter The {@code FilterCriteria}
+     */
     private static void updateStatus(FilterCriteria filter) {
         System.out.println("Select Status:");
         System.out.println("1. PENDING\n2. APPROVED\n3. REJECTED\n4. FILLED\n5. CLEAR");
@@ -58,6 +73,11 @@ public class FilterUI {
         InputHelper.pause();
     }
 
+    /**
+     * accepts user input to save prefered major or
+     * accept 0 to make major = null
+     * @param filter The {@code FilterCriteria}
+     */
     private static void updateMajor(FilterCriteria filter) {
         String major = AttributeGetter.getString("Enter preferred major (or '0' to clear): ");
         if (major.equals("0")) filter.setPreferredMajor(null);
@@ -66,6 +86,11 @@ public class FilterUI {
         InputHelper.pause();
     }
 
+    /**
+     * Displays a menu of possible internhsip level options,
+     * accepts user input to select the level
+     * @param filter The {@code FilterCriteria}
+     */
     private static void updateLevel(FilterCriteria filter) {
         System.out.println("Select Internship Level:");
         System.out.println("1. BASIC\n2. INTERMEDIATE\n3. ADVANCED\n4. CLEAR");
@@ -83,6 +108,10 @@ public class FilterUI {
         InputHelper.pause();
     }
 
+    /**
+     *  accepts user input and set closing date or null it when = 0
+     * @param filter The {@code FilterCriteria}
+     */
     private static void updateClosingDate(FilterCriteria filter) {
         String dateInput = AttributeGetter.getString("Enter closing date (yyyy-MM-dd) or 0 to clear: ");
         if (dateInput.equals("0")) {
@@ -98,6 +127,10 @@ public class FilterUI {
         InputHelper.pause();
     }
 
+    /**
+     *clear all filter criteria by setting all the criteria to null
+     * @param filter The {@code FilterCriteria}
+     */
     private static void clearAll(FilterCriteria filter) {
         filter.setStatus(null);
         filter.setPreferredMajor(null);
@@ -107,6 +140,11 @@ public class FilterUI {
         InputHelper.pause();
     }
 
+    /**
+     * Converts the provided object to its string
+     * @param obj The object to be converted
+     * @return The string
+     */
     private static String display(Object obj) {
         return obj == null ? "None" : obj.toString();
     }
